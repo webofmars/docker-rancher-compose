7511112bf106286de51514b25d85371a65c327bd
Andrzej Ressel <andrzej.ressel@identt.pl>
2017-04-04 14:25:31 +0200

    Don't reverse sort

--- generator.sh
+++ generator.sh
@@ -3,4 +3,4 @@
 while read VERSION; do
   docker build --pull -t $IMAGE_NAME:$VERSION --build-arg RANCHER_COMPOSE_VERSION=$VERSION .
   docker push $IMAGE_NAME:$VERSION
-done < <(git ls-remote https://github.com/rancher/rancher-compose.git | grep -o 'refs/tags/v.*' | sed 's:refs/tags/v::' | sort -Vr)
+done < <(git ls-remote https://github.com/rancher/rancher-compose.git | grep -o 'refs/tags/v.*' | sed 's:refs/tags/v::' | sort -V)
